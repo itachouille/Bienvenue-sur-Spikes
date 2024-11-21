ce05924e85276256d338609ca8c9849339e7303e
Anthony David <anthony.david34@sfr.fr>
2024-11-21 11:06:01 +0100

    image fix

--- app/components/Card.tsx
+++ app/components/Card.tsx
@@ -1,3 +1,5 @@
+import Image from "next/image";
+
 interface CardProps {
   profilePic: string;
   name: string;
@@ -10,7 +12,12 @@ const Card = ({ profilePic, name, username, text }: CardProps) => {
     <div className="relative w-72 h-40 rounded-xl p-px bg-gradient-to-b from-[#4C3182] to-[#11032C]">
       <div className="rounded-xl py-4 px-6 bg-[#0C0024] h-full">
         <div className="flex items-center gap-2">
-          <img src={profilePic} alt="profile Picture" className="size-8" />
+          <Image
+            src={profilePic}
+            width={32}
+            height={32}
+            alt="profile Picture"
+          />
           <div className="flex flex-col">
             <span className="text-white font-medium">{name}</span>
             <span className="text-[#807FBC] text-xs">{username}</span>
